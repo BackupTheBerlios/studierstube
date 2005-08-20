a9d95124413865b8acea00cf93527bf9bc35a9a6
twel <twel>
2005-08-20 11:16:46 +0000

    just a test

--- studierstube/studierstube/tests/Test.java
+++ studierstube/studierstube/tests/Test.java
@@ -29,5 +29,7 @@ public class Test {
   	  artefakt.neuerAusloeser(artefakt.new Ausloeser(i, "Fingerschnippen", "einmalig"));
   	}
     Global.out("ergebnis == " + artefakt.zaehleAusloeser());
+    
+    Global.Fehler("aaaaaaaa");
   }
 }
