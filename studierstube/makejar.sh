46e6dc57680380273cf739a07fb748d672d318d5
twel <twel>
2005-09-17 19:21:30 +0000

    zauber.xml funktioniert nicht im JAR

--- studierstube/makejar.sh
+++ studierstube/makejar.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-echo "Manifest-Version: 1.0" > manifest.txt
-echo "Main-Class: studierstube.Main" >> manifest.txt
-jar cfm studierstube.jar manifest.txt studierstube/
+rm studierstube.jar
+echo "Manifest-Version: 1.0" > manifest.tmp
+echo "Main-Class: studierstube.Main" >> manifest.tmp
+find studierstube/ -name "*.class" > filelist.tmp
+find -name "*.xml" >> filelist.tmp
+jar cfm studierstube.jar manifest.tmp @filelist.tmp
+rm manifest.tmp filelist.tmp
