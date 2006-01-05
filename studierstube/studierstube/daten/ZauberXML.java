e7b3a610e2909f0b642b9c925b4eb7e8eea5e90d
twel <twel>
2006-01-05 12:00:18 +0000

    Copyright 2005-2006 Doku aktualisiert

--- studierstube/studierstube/daten/ZauberXML.java
+++ studierstube/studierstube/daten/ZauberXML.java
@@ -1,6 +1,6 @@
 /**************************************************************
 * This file is part of the project 'Studierstube' at berliOS. *
-* Copyright (c) 2005 Stefan Holzmüller (twelwan@gmx.de)       *
+* Copyright (c) 2005-2006 Stefan Holzmüller (twelwan@gmx.de)  *
 * The software and its sources are available under the terms  *
 * of the zlib/libpng license. See LICENSE.txt for details.    *
 **************************************************************/
